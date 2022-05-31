00dfd6237dde83c01ca526ef6231fb7ce0862bd9
Andrey Nazarov <skuller@skuller.net>
2022-06-01 00:53:56 +0300

    Fix off-by-one error in Info_SetValueForKey().

--- q_shared.c
+++ q_shared.c
@@ -1497,7 +1497,7 @@ void Info_SetValueForKey (char *s, const char *key, const char *value)
 
 	Com_sprintf (newi, sizeof(newi), "\\%s\\%s", key, value);
 
-	if (strlen(newi) + strlen(s) > MAX_INFO_STRING)
+	if (strlen(newi) + strlen(s) > MAX_INFO_STRING-1)
 	{
 		Com_Printf ("Info string length exceeded while trying to set '%s'\n", LOG_GENERAL, newi);
 		return;
